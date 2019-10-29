17562b869df434bf2e0f4cf2469a3d34beed2bca
Yisus TTNK <jaguilar12@yahoo.com>
2019-10-29 03:35:10 -0600

    #2

--- cl_list/settings.py
+++ cl_list/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +27,7 @@ SECRET_KEY = '@ghv#y--b=eb&k8gr+e-3y&un+g61uxfp&ur@ci_o*ea4(t_@!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dry-beyond-68941.herokuapp.com']
 
 
 # Application definition
@@ -121,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+django_heroku.settings(locals())
